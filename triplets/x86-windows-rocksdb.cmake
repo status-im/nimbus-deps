1d4f00a82cbddfb48f95597e55987a601acfc23e
cheatfate <ka@hardcore.kiev.ua>
2018-07-27 22:06:52 +0300

    Change rocksdb config to include all compression libraries statically. Add building of snappy.dlls.

---
+++ triplets/x86-windows-rocksdb.cmake
@@ -0,0 +1,9 @@
+set(VCPKG_TARGET_ARCHITECTURE x86)
+
+if(${PORT} MATCHES "snappy|zlib|lz4")
+  set(VCPKG_CRT_LINKAGE static)
+  set(VCPKG_LIBRARY_LINKAGE static)
+else()
+  set(VCPKG_CRT_LINKAGE static)
+  set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
