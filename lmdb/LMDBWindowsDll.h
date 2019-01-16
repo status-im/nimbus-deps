48b37330c19e5a8a1a9d0f760636f6567d3671fd
andri lim <jangko128@gmail.com>
2019-01-16 22:17:21 +0700

    add lmdb

---
+++ lmdb/LMDBWindowsDll.h
@@ -0,0 +1,13 @@
+// The following ifdef block is the standard way of creating macros which make exporting
+// from a DLL simpler. All files within this DLL are compiled with the LMDBWINDOWSDLL_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see
+// LMDBWINDOWSDLL_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef LMDBWINDOWSDLL_EXPORTS
+#define LMDBWINDOWSDLL_API __declspec(dllexport)
+#else
+#define LMDBWINDOWSDLL_API __declspec(dllimport)
+#endif
+
+#pragma comment (lib, "ntdll.lib")
